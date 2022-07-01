e7583f9c1ad65ef8d500b641899167a9178346f9
edgar84 <edgar.tomko.97@mail.ru>
2022-07-01 06:53:37 +0300

    creating a handler for an image

--- account/views.py
+++ account/views.py
@@ -54,11 +54,11 @@ def register(request):
 @login_required
 def edit(request):
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user, data=request.POST)
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
         profile_form = ProfileEditForm(instance=request.user.profile,
                                        data=request.POST,
                                        files=request.FILES)
-
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
@@ -68,5 +68,7 @@ def edit(request):
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-    return render(request, 'account/edit.html',
-                  {'user_form': user_form, 'profile_form': profile_form})
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
