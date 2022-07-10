209e51b47d99c5a9288f7a7b8b0b33519a01c49c
edgar84 <edgar.tomko.97@mail.ru>
2022-07-10 09:57:16 +0300

    create Contact model from account

--- images/views.py
+++ images/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse, HttpResponse
-from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+
+from common.decorators import ajax_required, is_ajax
 from .forms import ImageCreateForm
 from .models import Image
-from common.decorators import ajax_required, is_ajax
 
 
 @login_required
